24429a82a1d2d854e79f57d55af33b78db3da7ff
Dmitry Serzhputovski <123190027+dedserch@users.noreply.github.com>
2025-03-12 13:28:30 +0300

    Refactor: abiturient entity

--- src/main/java/com/serzhputovski/lab1/entity/Abiturient.java
+++ src/main/java/com/serzhputovski/lab1/entity/Abiturient.java
@@ -31,7 +31,6 @@ public class Abiturient {
         return name;
     }
 
-
     public String getPatronymic() {
         return patronymic;
     }
@@ -47,15 +46,45 @@ public class Abiturient {
     public int getGrade() {
         return grade;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Abiturient that = (Abiturient) o;
+
+        if (id != that.id) return false;
+        if (grade != that.grade) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (patronymic != null ? !patronymic.equals(that.patronymic) : that.patronymic != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        return phone != null ? phone.equals(that.phone) : that.phone == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (patronymic != null ? patronymic.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + grade;
+        return result;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Abiturient{");
         sb.append("id=").append(id);
         sb.append(", surname='").append(surname).append('\'');
-        sb.append(", name=").append(name);
+        sb.append(", name='").append(name).append('\'');
         sb.append(", patronymic='").append(patronymic).append('\'');
-        sb.append(", address=").append(address);
-        sb.append(", phone=").append(phone);
+        sb.append(", address='").append(address).append('\'');
+        sb.append(", phone='").append(phone).append('\'');
         sb.append(", grade=").append(grade);
         sb.append('}');
         return sb.toString();
